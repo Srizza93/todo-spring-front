4e6a3fc54598d0f7bd27d292c9c6f6761ca0edef
Simone Rizza <simonerizza@mbp-de-simone.home>
2023-05-03 15:53:39 +0200

    build(api): modal shadow

--- src/components/userProfile/TodoModal.vue
+++ src/components/userProfile/TodoModal.vue
@@ -69,6 +69,7 @@ function formatDate(date: Date): string {
     border-radius: $radius;
     background-color: $third-color;
     z-index: 9;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
 }
 
 .modal_close-button {

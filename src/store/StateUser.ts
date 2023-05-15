81ac70a991ec2c5b8f39aa0b6cf77936b569820c
Simone Rizza <simonerizza@mbp-de-simone.home>
2023-05-15 23:37:36 +0200

    refactor(lang): store handling

--- src/store/StateUser.ts
+++ src/store/StateUser.ts
@@ -4,7 +4,8 @@ export const useStateUserStore = defineStore('stateUser', {
     state: () => ({
         token: '' as string,
         userId: '' as string,
-        loggedIn: false as boolean
+        loggedIn: false as boolean,
+        language: '' as string
     }),
     actions: {
         updateToken(newToken: string): void {
@@ -13,6 +14,9 @@ export const useStateUserStore = defineStore('stateUser', {
         updateUserId(newId: string): void {
             this.userId = newId
         }, 
+        updateLanguage(newLang: string): void {
+            this.language = newLang
+        },
         setUserStatus(status: boolean): void {
             this.loggedIn = status
         } 

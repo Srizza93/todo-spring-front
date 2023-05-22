25bd907b56d54cfd3158d1d6b96052de88a4ddd4
Simone Rizza <simonerizza@mbp-de-simone.home>
2023-05-22 20:23:30 +0200

    test(setup): todos api

--- src/api/Todos.ts
+++ src/api/Todos.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import { SelectedTodos, Todo } from '../types/TodoType'
 import { useStateUserStore } from '../store/StateUser'
-const store = useStateUserStore()
 
 export async function getTodos(todoType: SelectedTodos, userId: string): Promise<any> {
+  const store = useStateUserStore()
     return axios.get(`/api/todos/${todoType}/${userId}`, {
       headers: {
         Authorization: `Bearer ${store.$state.token}`
@@ -14,31 +14,34 @@ export async function getTodos(todoType: SelectedTodos, userId: string): Promise
 }
 
 export async function postTodo(todo: Todo): Promise<any> {
+  const store = useStateUserStore()
     return axios.post(`/api/todos`, todo, {
       headers: {
         Authorization: `Bearer ${store.$state.token}`
       }
     })
-      .then(res => res.data)
+      .then(res => res)
       .catch(console.error)
 }
 
 export async function putTodo(todo: Todo): Promise<any> {
+  const store = useStateUserStore()
     return axios.put(`/api/todos`, todo, {
       headers: {
         Authorization: `Bearer ${store.$state.token}`
       }
     })
-      .then(res => res.data)
+      .then(res => res)
       .catch(console.error)
 }
 
 export async function deleteTodo(id: number): Promise<any> {
+  const store = useStateUserStore()
     return axios.delete(`/api/todos/${id}`, {
       headers: {
         Authorization: `Bearer ${store.$state.token}`
       }
     })
-      .then(res => res.data)
+      .then(res => res)
       .catch(console.error)
 }
